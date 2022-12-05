62b8def5d77be8860acaabe79df67f2d53100e6e
Rémi Marçais <remi.marcais87@gmail.com>
2022-12-05 02:05:48 +0100

    Added comment and improved code

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -3,23 +3,27 @@
 
 
 def canUnlockAll(boxes):
-    """Method that determines if all the boxes can be opened."""
+    """Method that determines if all the boxes can be unlocked."""
 
     number_boxes = len(boxes)
 
     if boxes == []:
         return True
 
-    boxes[0].append("opened")
+    # We append "ul" (that stands for unlocked) to the boxe 0
+    boxes[0].append("ul")
 
     for boxe in boxes:
         index = boxes.index(boxe)
-        for item in boxe:
-            if (item != "opened" and item != index and item >= 0 and
-                    item < number_boxes and "opened" not in boxes[item]):
-                boxes[item].append("opened")
+        for key in boxe:
+            # If the key can unlock a locked box of the list 'boxes',
+            # we append "ul" to this box
+            if (key != "ul" and key != index and key >= 0 and
+                    key < number_boxes and "ul" not in boxes[key]):
+                boxes[key].append("ul")
 
+    # We return False if one of the boxes can't be unlocked
     for boxe in boxes:
-        if "opened" not in boxe:
+        if "ul" not in boxe:
             return False
     return True
