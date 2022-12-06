b9fde529d2431b55c4ec1874090f00cffda6de89
Rémi Marçais <remi.marcais87@gmail.com>
2022-12-07 00:35:43 +0100

    Removed print and improved code

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -7,9 +7,6 @@ def canUnlockAll(boxes):
 
     number_boxes = len(boxes)
 
-    if boxes == []:
-        return True
-
     # The first box is unlocked
     unlocked_boxes = [0]
 
@@ -17,10 +14,9 @@ def canUnlockAll(boxes):
         index = boxes.index(boxe)
         for key in boxe:
             # If the key can unlock a locked box of the list 'boxes',
-            # we append "ul" to this box
+            # we append the box number in "unlocked_boxes"
             if (key != index and key >= 0 and
                     key < number_boxes and key not in unlocked_boxes):
                 unlocked_boxes.append(key)
 
-    print(unlocked_boxes)
     return len(unlocked_boxes) == number_boxes
