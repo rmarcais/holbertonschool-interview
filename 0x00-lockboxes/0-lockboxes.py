d276dd2e04a3be09b6a32caf3e3708edfcb2eb00
Rémi Marçais <remi.marcais87@gmail.com>
2022-12-07 02:48:15 +0100

    Go back to the previous version

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -5,22 +5,45 @@
 def canUnlockAll(boxes):
     """Method that determines if all the boxes can be unlocked."""
 
-    number_boxes = len(boxes)
-
     if boxes == []:
         return True
 
-    # The first box is unlocked
-    unlocked_boxes = [0]
+    # When we get the key of a box, we put it in this list.
+    boxes_to_unlock = [0]
 
-    for boxe in boxes:
-        index = boxes.index(boxe)
-        for key in boxe:
-            # If the key can unlock a locked box of the list 'boxes',
-            # we append "ul" to this box
-            # we append the box number in "unlocked_boxes"
-            if (key != index and key >= 0 and
-                    key < number_boxes and key not in unlocked_boxes):
-                unlocked_boxes.append(key)
+    # This list contains the number of the boxes we can unlock.
+    # At this moment, no box can be unlocked.
+    unlocked_boxes = [False for box in boxes]
 
-    return len(unlocked_boxes) == number_boxes
+    # This means the box 0 can be unlocked
+    unlocked_boxes[0] = True
+
+    # While we can unlock boxes...
+    while len(boxes_to_unlock) > 0:
+        # We take the first key of boxes_to_unlock as index.
+        index = boxes_to_unlock[0]
+
+        # We check the keys of boxes[index]
+        # to know if we can unlock antoher box.
+        boxes_to_unlock = add_keys(boxes_to_unlock,
+                                   unlocked_boxes, boxes, index)
+
+        # We do this to mark the box at the index 'index' as unlocked.
+        unlocked_boxes[index] = True
+
+        # Once the box marked as unlocked,
+        # we can remove its key from boxes_to_unlock.
+        boxes_to_unlock.pop(0)
+
+    # We return True if all boxes can be unlocked.
+    return all(unlocked_boxes)
+
+
+def add_keys(boxes_to_unlock, unlocked_boxes, boxes, index):
+    """Method that adds keys to boxes_to_unlock"""
+    for key in boxes[index]:
+        # If boxes[key] is not marked as unlocked,
+        # we add its key to boxes_to_unlock
+        if key > 0 and key < len(boxes) and not unlocked_boxes[key]:
+            boxes_to_unlock.append(key)
+    return boxes_to_unlock
