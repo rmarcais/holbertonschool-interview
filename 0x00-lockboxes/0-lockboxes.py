e768e01e081c86132f53884263454d7728faf63d
Rémi Marçais <remi.marcais87@gmail.com>
2022-12-07 11:33:32 +0100

    Removed comments

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -8,36 +8,16 @@ def canUnlockAll(boxes):
     if boxes == []:
         return True
 
-    # When we get the key of a box, we put it in this list.
     boxes_to_unlock = [0]
-
-    # This list contains the number of the boxes we can unlock.
-    # At this moment, no box can be unlocked.
     unlocked_boxes = [False for box in boxes]
-
-    # This means the box 0 can be unlocked
     unlocked_boxes[0] = True
 
-    # While we can unlock boxes...
     while len(boxes_to_unlock) > 0:
-        # We take the first key of boxes_to_unlock as index.
         index = boxes_to_unlock[0]
-
-        # We check the keys of boxes[index]
-        # to know if we can unlock antoher box.
         for key in boxes[index]:
-            # If boxes[key] is not marked as unlocked,
-            # we add its key to boxes_to_unlock
             if (key < len(boxes) and not unlocked_boxes[key]
                     and key not in boxes_to_unlock):
                 boxes_to_unlock.append(key)
-
-        # We do this to mark the box at the index 'index' as unlocked.
         unlocked_boxes[index] = True
-
-        # Once the box marked as unlocked,
-        # we can remove its key from boxes_to_unlock.
         boxes_to_unlock.pop(0)
-
-    # We return True if all boxes can be unlocked.
     return all(unlocked_boxes)
