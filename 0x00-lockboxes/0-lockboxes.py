229ca188cdbda42dbfdb52ec2a115f36cff96c9e
Rémi Marçais <remi.marcais87@gmail.com>
2022-12-07 01:26:02 +0100

    Add a new version because the previous one didn't cover all cases

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -5,18 +5,33 @@
 def canUnlockAll(boxes):
     """Method that determines if all the boxes can be unlocked."""
 
-    number_boxes = len(boxes)
+    # When we get the key of a box, we put it in this list.
+    boxes_to_unlock = []
 
-    # The first box is unlocked
-    unlocked_boxes = [0]
+    # This list contains the number of the boxes we can unlock.
+    # At this moment, no box can be unlocked.
+    unlocked_boxes = [False for box in boxes]
 
-    for boxe in boxes:
-        index = boxes.index(boxe)
-        for key in boxe:
-            # If the key can unlock a locked box of the list 'boxes',
-            # we append the box number in "unlocked_boxes"
-            if (key != index and key >= 0 and
-                    key < number_boxes and key not in unlocked_boxes):
-                unlocked_boxes.append(key)
+    # We add the keys contained in the box 0 to the list.
+    boxes_to_unlock = add_keys(boxes_to_unlock, unlocked_boxes, boxes, 0)
 
-    return len(unlocked_boxes) == number_boxes
+    # This means the box 0 can be unlocked
+    unlocked_boxes[0] = True
+
+    # While we can unlock boxes...
+    while len(boxes_to_unlock) > 0:
+        index = boxes_to_unlock[0]
+        boxes_to_unlock = add_keys(boxes_to_unlock, unlocked_boxes, boxes, index)
+        boxes_to_unlock.pop(0)
+        print(type(boxes_to_unlock))
+        unlocked_boxes[index] = True
+        print(unlocked_boxes)
+    return all(unlocked_boxes)
+    
+
+def add_keys(boxes_to_unlock, unlocked_boxes, boxes, index):
+    """Method that adds keys to boxes_to_unlock"""
+    for key in boxes[index]:
+        if boxes[key] not in unlocked_boxes:
+            boxes_to_unlock.append(key)
+    return boxes_to_unlock
