74dc8320bee78dd42381c901e9933783fd17bf67
Rémi Marçais <remi.marcais87@gmail.com>
2023-01-24 23:07:39 +0100

    Added all files from task 0

---
+++ 0x06-log_parsing/0-stats.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Task 0. Log parsing"""
+
+from sys import stdin
+import re
+import signal
+
+lap = 0
+
+status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+
+regex = r"^\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3} - "
+regex += r"\[[\d\-:\.\s]+\] \"[\w\s\/.]+\" \d{1,3} \d{1,3}$"
+
+total_size = 0
+
+
+def print_stats(status_codes, total_size):
+    print("File size: {}".format(total_size))
+    for k, v in status_codes.items():
+        if v > 0:
+            print("{}: {}".format(k, v))
+
+def handler(signal, frame):
+    print_stats(status_codes, total_size)
+
+signal.signal(signal.SIGINT, handler)
+
+for line in stdin:
+    if re.match(regex, line):
+        lap += 1
+        infos = line.split()
+        code = int(infos[-2])
+        total_size += int(infos[-1])
+        if code in status_codes.keys():
+            status_codes[code] += 1
+        if lap == 10:
+            lap = 0
+            print_stats(status_codes, total_size)
