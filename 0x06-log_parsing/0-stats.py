d242db5ed46e4d2fe3f08130aecf887b0010a51c
Rémi Marçais <remi.marcais87@gmail.com>
2023-01-25 10:48:45 +0100

    Added a new print_stats call

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -3,14 +3,14 @@
 
 from sys import stdin
 import re
-import signal
+from time import sleep
 
 lap = 0
 
 status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 
 regex = r"^\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3} - "
-regex += r"\[[\d\-:\.\s]+\] \"[\w\s\/.]+\" \d{1,3} \d{1,3}$"
+regex += r"\[[\d\-:\.\s]+\] \"[\w\s\/.]+\" \d{1,3} \d{1,3}"
 
 total_size = 0
 
@@ -23,22 +23,20 @@ def print_stats(status_codes, total_size):
             print("{}: {}".format(k, v))
 
 
-def handler(signal, frame):
+if __name__ == "__main__":
+    try:
+        for line in stdin:
+            if re.match(regex, line):
+                lap += 1
+                infos = line.split()
+                code = int(infos[-2])
+                total_size += int(infos[-1])
+                if code in status_codes.keys():
+                    status_codes[code] += 1
+                if lap == 10:
+                    lap = 0
+                    print_stats(status_codes, total_size)
+    except KeyboardInterrupt:
+        print_stats(status_codes, total_size)
+        raise
     print_stats(status_codes, total_size)
-    raise KeyboardInterrupt
-
-
-signal.signal(signal.SIGINT, handler)
-
-
-for line in stdin:
-    if re.match(regex, line):
-        lap += 1
-        infos = line.split()
-        code = int(infos[-2])
-        total_size += int(infos[-1])
-        if code in status_codes.keys():
-            status_codes[code] += 1
-        if lap == 10:
-            lap = 0
-            print_stats(status_codes, total_size)
