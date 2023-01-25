8ffba0fc569afc466d9ffc8c8b5e087ed4c23cb5
Rémi Marçais <remi.marcais87@gmail.com>
2023-01-25 11:38:31 +0100

    Tried to fix bugs

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -1,34 +1,27 @@
 #!/usr/bin/python3
-"""Task 0. Log parsing"""
-
+""" Write a function to print some informations """
 import sys
-
-lap = 0
-
-status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-
+count = 0
 total_size = 0
-
-
-def print_stats():
-    """Prints statistics from the beginning"""
+codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+def print_line():
+    """ Print some information """
     print("File size: {}".format(total_size))
-    for k, v in status_codes.items():
-        if v > 0:
-            print("{}: {}".format(k, v))
-
-
-if __name__ == "__main__":
+    for code in sorted(codes.keys()):
+        if codes[code] > 0:
+            print("{}: {}".format(code, codes[code]))
+if __name__ == '__main__':
     try:
         for line in sys.stdin:
-            lap += 1
-            infos = line.split()
-            code = int(infos[-2])
-            if code in status_codes.keys():
-                total_size += int(infos[-1])
-                status_codes[code] += 1
-            if lap % 10 == 0:
-                print_stats()
+            split_line = line.split()
+            size = int(split_line[-1])
+            status = int(split_line[-2])
+            if status in codes.keys():
+                count += 1
+                total_size += size
+                codes[status] += 1
+            if count % 10 == 0:
+                print_line()
     except KeyboardInterrupt:
-        print_stats()
-    print_stats()
+        print_line()
+    print_line()
