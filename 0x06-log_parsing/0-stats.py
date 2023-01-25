7b0e649a1b63e4452c19d98d86f4266b428441e2
Rémi Marçais <remi.marcais87@gmail.com>
2023-01-25 11:31:55 +0100

    Tried to fix bugs

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -3,7 +3,6 @@
 
 from sys import stdin
 
-
 lap = 0
 
 status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
@@ -26,9 +25,9 @@ if __name__ == "__main__":
             infos = line.split()
             total_size += int(infos[-1])
             code = int(infos[-2])
-            status_codes[code] += 1
-            if lap == 10:
-                lap = 0
+            if code in status_codes.keys():
+                status_codes[code] += 1
+            if lap % 10 == 0:
                 print_stats()
     except KeyboardInterrupt:
         print_stats()
