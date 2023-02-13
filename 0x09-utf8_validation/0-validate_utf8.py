3481a9518928b38e4970c6af1737e080107e193c
Rémi Marçais <remi.marcais87@gmail.com>
2023-02-13 09:45:16 +0100

    Added 0-validate_utf8.py

---
+++ 0x09-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""0-validate_utf8 module"""
+
+from typing import List
+
+
+def count_ones(n: int) -> int:
+    """Counts the ones at the beginning of a number"""
+    count = 0
+    for i in range(7, -1, -1):
+        if n & (1 << i):
+            count += 1
+        else:
+            break
+    return count
+
+
+def validUTF8(data: List[int]) -> bool:
+    """
+    Determines if a given data set represents
+    a valid UTF-8 encoding
+    """
+    count = 0
+    for d in data:
+        if not count:
+            count = count_ones(d)
+            if count == 0:
+                continue
+            elif count == 1 or count > 4:
+                return False
+        else:
+            if count_ones(d) != 1:
+                return False
+        count -= 1
+    return True
