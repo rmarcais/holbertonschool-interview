7f5ee37829c827111d318c0e97dc4e211ccd1590
Rémi Marçais <remi.marcais87@gmail.com>
2023-04-19 09:27:41 +0200

    Go back to the prvious version

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -7,4 +7,17 @@ def rain(walls):
     Function that calculates how many square units
     of water will be retained after it rains
     """
-    return 0
+    if not walls or len(walls) == 0:
+        return 0
+
+    result = 0
+    wall_height = 0
+    rain = 0
+    for wall in walls:
+        if wall == 0:
+            rain += 1
+        else:
+            result += min(wall_height, wall) * rain
+            rain = 0
+            wall_height = wall
+    return result
