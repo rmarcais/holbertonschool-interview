47b49c258435938bca330c29a8cc938a2337b136
Rémi Marçais <remi.marcais87@gmail.com>
2023-04-19 09:05:05 +0200

    Try to fix issue

--- 0x10-rain/0-rain.py
+++ 0x10-rain/0-rain.py
@@ -9,13 +9,17 @@ def rain(walls):
     """
     if not walls or len(walls) == 0:
         return 0
+    
+    if all(w == max(walls) for w in walls):
+        if max(walls) == 0:
+            return 0
+        else:
+            return sum(walls) - 2
 
     result = 0
     wall_height = 0
     rain = 0
     for wall in walls:
-        if not isinstance(wall, int):
-            return 0
         if wall == 0:
             rain += 1
         else:
