ef5746874ab9339bc17ed08395456d179a0cbb85
Rémi Marçais <remi.marcais87@gmail.com>
2023-06-12 10:55:00 +0200

    Implement the function

--- 0x17-find_the_loop/0-find_loop.c
+++ 0x17-find_the_loop/0-find_loop.c
@@ -2,7 +2,7 @@
 
 /**
  * find_listint_loop - finds the loop in a linked list.
- * 
+ *
  * @head: A pointer to the first element of a list
  *
  * Return: the address of the node where
@@ -10,4 +10,26 @@
  */
 listint_t *find_listint_loop(listint_t *head)
 {
+	listint_t *slow = head, *fast = head;
+
+	while (fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (fast == slow)
+			break;
+	}
+
+	if (!fast || !fast->next)
+		return (NULL);
+
+	slow = head;
+
+	while (slow != fast)
+	{
+		slow = slow->next;
+		fast = fast->next;
+	}
+
+	return (slow);
 }
