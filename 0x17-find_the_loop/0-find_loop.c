ac970a77afdfb1fc8866c27c9cff4675d9f356ba
Rémi Marçais <remi.marcais87@gmail.com>
2023-06-12 10:16:53 +0200

    Add all needed files

---
+++ 0x17-find_the_loop/0-find_loop.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop - finds the loop in a linked list.
+ * 
+ * @head: A pointer to the first element of a list
+ *
+ * Return: the address of the node where
+ * the loop starts, or NULL if there is no loop.
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+}
