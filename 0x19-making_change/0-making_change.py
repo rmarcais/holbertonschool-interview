914dbded6e1eeea51ebb573543eff7e72eebd747
Rémi Marçais <remi.marcais87@gmail.com>
2023-07-05 01:37:10 +0200

    Fix issue

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -8,7 +8,21 @@ def makeChange(coins, total):
     to meet a given total total
     """
 
-    return min(1, 2)
+    if total < 1:
+        return 0
 
     
-    
+    dp = [-1 for i in range(0, total + 1)]
+
+    for i in coins:
+        if i > len(dp) - 1:
+            continue
+        dp[i] = 1
+        for j in range(i + 1, total + 1):
+            if dp[j - i] == -1:
+                continue
+            elif dp[j] == -1:
+                dp[j] = dp[j - i] + 1
+            else:
+                continue
+    return dp[total]
