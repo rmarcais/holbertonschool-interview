666bef7f7f0d0b6377dcb091c9c1681bf02eb7b7
Rémi Marçais <remi.marcais87@gmail.com>
2023-07-05 01:26:48 +0200

    Try to fix checker's issue

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -7,11 +7,3 @@ def makeChange(coins, total):
     Determines the fewest number of coins needed
     to meet a given total total
     """
-    if total < 1:
-        return 0
-    count = 0
-    for coin in coins:
-        while total >= coin:
-            total -= coin
-            count += 1
-    return count
