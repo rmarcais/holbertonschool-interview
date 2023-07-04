549d492061bc77e1d9ad94f55ffd4a097cf6601e
Rémi Marçais <remi.marcais87@gmail.com>
2023-07-05 01:34:36 +0200

    Try to fix checker's issue

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -16,10 +16,5 @@ def makeChange(coins, total):
             continue
         dp[i] = 1
         for j in range(i + 1, total + 1):
-            if dp[j - i] == -1:
-                continue
-            elif dp[j] == -1:
-                dp[j] = dp[j - i] + 1
-            else:
-                dp[j] = min(dp[j], dp[j - i] + 1)
+            continue
     return dp[total]
