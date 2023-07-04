5a45ec8b7ca62361a47c4733755d6a66645b1522
Rémi Marçais <remi.marcais87@gmail.com>
2023-07-05 01:09:11 +0200

    Try with a new implementation

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -7,22 +7,13 @@ def makeChange(coins, total):
     Determines the fewest number of coins needed
     to meet a given total total
     """
-
-    if total < 1:
+    if total == 0:
         return 0
-
-    # dp for dynamic programming
-    dp = [-1 for i in range(0, total + 1)]
-
-    for i in coins:
-        if i > len(dp) - 1:
-            continue
-        dp[i] = 1
-        for j in range(i + 1, total + 1):
-            if dp[j - i] == -1:
-                continue
-            elif dp[j] == -1:
-                dp[j] = dp[j - i] + 1
-            else:
-                dp[j] = min(dp[j], dp[j - i] + 1)
-    return dp[total]
+    count = 0
+    for coin in sorted(coins, reverse=True):
+        while total >= coin:
+            total -= coin
+            count += 1
+    if total != 0:
+        return -1
+    return count
