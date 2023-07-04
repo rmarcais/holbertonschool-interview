c578acaf4ae14f848d163472559274f76f34556f
Rémi Marçais <remi.marcais87@gmail.com>
2023-07-05 01:41:13 +0200

    Go back to the previous version

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -10,11 +10,19 @@ def makeChange(coins, total):
 
     if total < 1:
         return 0
-    count = 0
-    if total == 1:
-        count = 1
-    elif total == 2:
-        count = 2
-    else:
-        count = 3
-    return count
+
+    # dp for dynamic programming
+    dp = [-1 for i in range(0, total + 1)]
+
+    for i in coins:
+        if i > len(dp) - 1:
+            continue
+        dp[i] = 1
+        for j in range(i + 1, total + 1):
+            if dp[j - i] == -1:
+                continue
+            elif dp[j] == -1:
+                dp[j] = dp[j - i] + 1
+            else:
+                dp[j] = min(dp[j], dp[j - i] + 1)
+    return dp[total]
