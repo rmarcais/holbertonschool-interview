b589773575d538be9f60444d0d16f7c3d0f43429
Rémi Marçais <remi.marcais87@gmail.com>
2023-07-05 12:25:37 +0200

    Final version

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -2,37 +2,27 @@
 """Making Change"""
 
 
-import sys
- 
-# m is size of coins array (number of
-# different coins)
-def makeChange(coins, V):
-    m = len(coins)
-    # table[i] will be storing the minimum
-    # number of coins required for i value.
-    # So table[V] will have result
-    table = [0 for i in range(V + 1)]
- 
-    # Base case (If given value V is 0)
-    table[0] = 0
- 
-    # Initialize all table values as Infinite
-    for i in range(1, V + 1):
-        table[i] = sys.maxsize
- 
-    # Compute minimum coins required
-    # for all values from 1 to V
-    for i in range(1, V + 1):
-         
-        # Go through all coins smaller than i
-        for j in range(m):
-            if (coins[j] <= i):
-                sub_res = table[i - coins[j]]
-                if (sub_res != sys.maxsize and
-                    sub_res + 1 < table[i]):
-                    table[i] = sub_res + 1
-     
-    if table[V] == sys.maxsize:
-        return -1
-       
-    return table[V]
+def makeChange(coins, total):
+    """
+    Determines the fewest number of coins needed
+    to meet a given total total
+    """
+
+    if total < 1:
+        return 0
+
+    # dp for dynamic programming
+    dp = [-1 for i in range(0, total + 1)]
+
+    for i in coins:
+        if i > len(dp) - 1:
+            continue
+        dp[i] = 1
+        for j in range(i + 1, total + 1):
+            if dp[j - i] == -1:
+                continue
+            elif dp[j] == -1:
+                dp[j] = dp[j - i] + 1
+            else:
+                dp[j] = min(dp[j], dp[j - i] + 1)
+    return dp[total]
