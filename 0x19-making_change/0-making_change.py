5ab1217a5b502bc4402861efa0ae6e1161f5d1be
Rémi Marçais <remi.marcais87@gmail.com>
2023-07-05 10:40:46 +0200

    Go back to the previous version

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -6,18 +6,13 @@ def makeChange(coins, total):
     """
     make change: bottom up method
     """
-    if total <= 0:
+    if total < 1:
         return 0
-    if total in coins:
-        return 1
-    if len(coins) <= 0:
-        return -1
-    dp = [0] * (total + 1)
-    for i in range(1, total + 1):
-        dp[i] = total + 1
-    dp[0] = 0
-    for a in range(1, total + 1):
-        for coin in coins:
-            if a - coin >= 0:
-                dp[a] = min(dp[a], 1 + dp[a - coin])
-    return dp[total] if dp[total] != total + 1 else -1
+    count = 0
+    for coin in sorted(coins, reverse=True):
+        while total >= coin:
+            count += 1
+            total -= coin
+            if total == 0:
+                return count
+    return -1
