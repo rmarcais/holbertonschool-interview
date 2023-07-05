ede3167c6d1caf7f5f2b2b16bebe5b2a7e2cad48
Rémi Marçais <remi.marcais87@gmail.com>
2023-07-05 11:13:38 +0200

    Go back to the first version

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -4,15 +4,25 @@
 
 def makeChange(coins, total):
     """
-    make change: bottom up method
+    Determines the fewest number of coins needed
+    to meet a given total total
     """
+
     if total < 1:
         return 0
-    count = 0
-    for coin in sorted(coins, reverse=True):
-        while total >= coin:
-            count += 1
-            total -= coin
-            if total == 0:
-                return count
-    return -1
+
+    # dp for dynamic programming
+    dp = [-1 for i in range(0, total + 1)]
+
+    for i in coins:
+        if i > len(dp) - 1:
+            continue
+        dp[i] = 1
+        for j in range(i + 1, total + 1):
+            if dp[j - i] == -1:
+                continue
+            elif dp[j] == -1:
+                dp[j] = dp[j - i] + 1
+            else:
+                dp[j] = min(dp[j], dp[j - i] + 1)
+    return dp[total]
