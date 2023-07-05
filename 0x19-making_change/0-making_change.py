01590fd4d222d087aa203cfda8f97b1c0c0e22ed
Rémi Marçais <remi.marcais87@gmail.com>
2023-07-05 10:12:49 +0200

    Try with another code

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -3,19 +3,18 @@
 
 
 def makeChange(coins, total):
-    """
-    Determines the fewest number of coins needed
-    to meet a given total total
-    """
+    """Given a pile of coins of different values, determine the fewest
+    number of coins needed to meet a given amount total."""
 
-    if total < 1:
+    if total <= 0:
         return 0
-
+    coins.sort(reverse=True)
     count = 0
-    for coin in sorted(coins, reverse=True):
-        while total >= coin:
-            count += 1
-            total -= coin
-            if total == 0:
-                return count
-    return -1
+    for coin in coins:
+        if total <= 0:
+            break
+        count += total // coin
+        total = total % coin
+    if total != 0:
+        return -1
+    return count
