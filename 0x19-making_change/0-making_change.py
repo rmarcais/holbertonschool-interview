d337f7686be930d0937ed81d6efbc10a008a3c65
Rémi Marçais <remi.marcais87@gmail.com>
2023-07-05 14:17:02 +0200

    Go back to the previous version

--- 0x19-making_change/0-making_change.py
+++ 0x19-making_change/0-making_change.py
@@ -8,15 +8,21 @@ def makeChange(coins, total):
     to meet a given total total
     """
 
-    if total <= 0:
+    if total < 1:
         return 0
 
-    changes = [total + 1] * (total + 1)
-    changes[0] = 0
+    # dp for dynamic programming
+    dp = [-1 for i in range(0, total + 1)]
 
-    for i in range(1, total + 1):
-        for j in range(0, len(coins)):
-            if coins[j] <= i:
-                changes[i] = min(changes[i], changes[i - coins[j]] + 1)
-
-    return -1 if changes[total] > total else changes[total]
+    for i in coins:
+        if i > len(dp) - 1:
+            continue
+        dp[i] = 1
+        for j in range(i + 1, total + 1):
+            if dp[j - i] == -1:
+                continue
+            elif dp[j] == -1:
+                dp[j] = dp[j - i] + 1
+            else:
+                dp[j] = min(dp[j], dp[j - i] + 1)
+    return dp[total]
