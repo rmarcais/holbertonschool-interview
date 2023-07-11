1459a5b200e79c301cd38c2c478899d83a560cf7
Rémi Marçais <remi.marcais87@gmail.com>
2023-07-11 13:24:19 +0200

    Add all files from task 0

---
+++ 0x1A-double_circular_linked_list/0-add_node.c
@@ -0,0 +1,79 @@
+#include "list.h"
+#include <stdio.h>
+
+/**
+ * add_node_end - Adds a new node to the end of
+ * a double circular linked list.
+ *
+ * @list: the list to modify
+ * @str: the string to copy into the new node
+ *
+ * Return: the address of the new node, or NULL on failure.
+ */
+List *add_node_end(List **list, char *str)
+{
+	List *new_node, *last_node;
+
+	if (!list)
+		return (NULL);
+
+	new_node = malloc(sizeof(List));
+	if (!new_node)
+		return (NULL);
+
+	new_node->str = strdup(str);
+
+	if (!(*list))
+	{
+		new_node->next = new_node->prev = new_node;
+		*list = new_node;
+		return (new_node);
+	}
+
+	last_node = (*list)->prev;
+	new_node->next = *list;
+	new_node->prev = last_node;
+	(*list)->prev = new_node;
+	last_node->next = new_node;
+
+	return (new_node);
+}
+
+/**
+ * add_nodeadd_node_begin_end - Adds a new node to the beginning
+ * of a double circular linked list.
+ *
+ * @list: the list to modify
+ * @str: the string to copy into the new node
+ *
+ * Return: the address of the new node, or NULL on failure.
+ */
+List *add_node_begin(List **list, char *str)
+{
+	List *new_node, *last_node;
+
+	if (!list)
+		return (NULL);
+
+	new_node = malloc(sizeof(List));
+	if (!new_node)
+		return (NULL);
+
+	new_node->str = strdup(str);
+
+	if (!(*list))
+	{
+		new_node->next = new_node->prev = new_node;
+		*list = new_node;
+		return (new_node);
+	}
+
+	last_node = (*list)->prev;
+	new_node->prev = last_node;
+	new_node->next = (*list);
+	(*list)->prev = new_node;
+	last_node->next = new_node;
+	(*list) = new_node;
+
+	return (new_node);
+}
