53bc0e4e1493a579338b6f9503625f1fb9411a24
Rémi Marçais <remi.marcais87@gmail.com>
2023-09-06 00:31:08 +0200

    Try to fix issue

--- 0x22-primegame/0-prime_game.py
+++ 0x22-primegame/0-prime_game.py
@@ -18,7 +18,13 @@ def isWinner(x, nums):
     player = "Maria"
 
     for n in nums:
+        dp = [False] * (n + 1)
+        dp[0] = True
         for i in range(1, n + 1):
+            if dp[i]:
+                for k in range(i, -4, -1):
+                    if not dp[k]:
+                        i = k
             if not isPrime(n) and i == n:
                 defeats[player] -= 1
                 player = "Maria"
@@ -26,6 +32,9 @@ def isWinner(x, nums):
             elif not isPrime(n):
                 continue
             else:
+                dp[i] = True
+                for j in range(2 * i, n + 1, i):
+                    dp[j] = True
                 if player == "Maria":
                     player = "Ben"
                 else:
